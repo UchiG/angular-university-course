d49960ac0794e29004afa2c60134e7a02135c910
Uchirai <uchirai.govere@gmail.com>
2023-06-29 06:28:35 +0200

    onCourseEdited is now onCoursesEdited

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { COURSES } from '../../db-data';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit {
+      
+    @Input()
+    course: Course;
 
-}
+    @Input()
+    cardIndex: number;
+
+    @Output()
+    courseSelected = new EventEmitter<Course>();
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+      
+    }
+
+    IsImageVisible() {
+      return this.course && this.course.iconUrl;
+    }
+
+    onCourseViewed() {
+      console.log("card component - button clicked...") 
+      this.courseSelected.emit(this.course);
+    }
+
+    cardClasses() {
+      if (this.course.category == 'BEGINNER') {
+        return 'beginner'
+      }
+    }
+
+    cardStyles() {
+      return {
+        'background-image': 'url(' + this.course.iconUrl + ')'
+      }
+    }
+  }
